b02fc3cb29353cb41e76de8c54203277900577a0
EgorSokurenko <72275136+EgorSokurenko@users.noreply.github.com>
2024-05-06 19:36:04 +0300

    fix constants

--- constants/orderConstants.js
+++ constants/orderConstants.js
@@ -1,9 +1,3 @@
-const RESIDENTIAL_TILE_INSTALLATION = 'Residential Tile Installation'
-const SHOWER_PAN_WORK = 'Shower Pan Work'
-const FLOOR_LEVELING = 'Floor leveling'
-const COMMERCIAL_TILE_INSTALLATION = 'Commercial Tile Installation'
-const TILE_REPAIR = 'Tile Repair'
-
 const projectKindEnum = [
     'Residential Tile Installation',
     'Shower Pan Work',
@@ -34,21 +28,10 @@ const contactMethodsEnum = [
 
 module.exports = {
     projectKindEnum,
-    RESIDENTIAL_TILE_INSTALLATION,
-    SHOWER_PAN_WORK,
-    FLOOR_LEVELING,
-    COMMERCIAL_TILE_INSTALLATION,
-    TILE_REPAIR,
-    IMMEDIATELY,
-    THREE_SIX_MONTHS,
-    SIX_MONTHS,
+
     projectPlanningEnum,
-    YES,
-    NO,
-    NOT_SURE_YET,
+
     homeMeasurementsEnum,
-    PHONE_CALL,
-    TEXT,
-    EMAIL,
+
     contactMethodsEnum
 }
